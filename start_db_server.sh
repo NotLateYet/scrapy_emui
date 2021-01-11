f7b26eade91119a31ac2040f8cd98d9c51d9a4f5
NotLateYet <niuyuhucs@gmail.com>
2021-01-11 12:31:40 +0800

    add remote access

--- start_db_server.sh
+++ start_db_server.sh
@@ -1,5 +1,8 @@
+# bind 0.0.0.0 for all remote client
+sudo sed -i 's/^bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
 # start redis service
-redis-server
+sudo systemctl restart redis-server
 
+sudo set -i 's/^bind_ip = 127.0.0.1/bin_ip = 0.0.0.0/g' /etc/mongdb.conf
 # start mongodb service
-systemctl start mongod
+sudo systemctl restart mongod
