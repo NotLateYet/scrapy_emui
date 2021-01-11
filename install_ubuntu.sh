85dd98b9ed4a376eafdc4537caa16e37bf910ad8
NotLateYet <niuyuhucs@gmail.com>
2021-01-11 10:44:50 +0800

    add shell for install redis&mongodb for ubuntu

---
+++ install_ubuntu.sh
@@ -0,0 +1,5 @@
+sudo su
+sudo apt-get update
+sudo apt-get install -y nmap
+sudo apt-get install -y redis-server
+sudo apt-get install -y mongodb-org
---
+++ start_db_server.sh
@@ -0,0 +1,5 @@
+# start redis service
+redis-server
+
+# start mongodb service
+systemctl start mongod
